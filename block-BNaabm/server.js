08ed5a6d29f673926a456bfc2eca26cb0a4eb608
ritik-cyber <ritikdhingra03@gmail.com>
2022-01-07 13:42:22 +0530

    done

--- block-BNaabm/server.js
+++ block-BNaabm/server.js
@@ -2,6 +2,15 @@ var express = require("express");
 
 var app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to express");
+});
+
 app.listen(3000, () => {
   console.log("3000 port is running");
 });
